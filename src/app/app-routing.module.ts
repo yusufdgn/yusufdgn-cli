110118ec775f20ac9d325b0af0519218df7950af
Yusuf Dogan <you@example.com>
2021-11-19 00:53:28 +0300

    dist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: '', component: DashboardComponent},
+  {path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
